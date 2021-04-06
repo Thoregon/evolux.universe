2d4a433bf9fe850ed43aef99b2e51731ee5c8302
Bernhard Lukassen <bl@bernhard-lukassen.com>
2021-04-06 17:41:00 +0200

    PULS intermediate

--- index.reliant.mjs
+++ index.reliant.mjs
@@ -6,11 +6,12 @@
 
 export *                                    from './common.mjs';
 
-export *                                    from './lib/reliant/busy.mjs';
-// export { default as protouniverse }         from './lib/reliant/protouniverse.mjs';
+// export *                                    from './lib/reliant/busy.mjs';       !fix
 export { default as unload }                from './lib/reliant/unload.mjs'
-export { default as serviceWorkerSetup }    from './lib/reliant/utils/serviceworkersetup.mjs';
-export { default as loadIframe }            from './lib/reliant/utils/iframeloader.mjs';
 
 import letThereBeLight                      from './lib/reliant/letThereBeLight.mjs';
 export default letThereBeLight;
+
+// previous modules:
+// export { default as serviceWorkerSetup }    from './lib/reliant/utils/serviceworkersetup.mjs';
+// export { default as loadIframe }            from './lib/reliant/utils/iframeloader.mjs';
