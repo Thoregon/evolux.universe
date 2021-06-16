50f7dee8835931183dfa8b1e12df153d8ff88f12
Bernhard Lukassen <bl@bernhard-lukassen.com>
2021-06-16 13:08:48 +0200

    refactored loaders, prepare for distributed repositories, fixes

--- bootloader.mjs
+++ bootloader.mjs
@@ -6,8 +6,9 @@
  * @author: blukassen
  */
 
-import Bootloader       from "./lib/sovereign/bootloader.mjs";
+import Bootloader       from "./lib/loader/bootloader.mjs";
 import process          from "process";
+import crypto           from 'crypto';
 
 const baseURL           = new URL(`${process.cwd()}/`, 'file://');
 const bootloader        = new Bootloader();
@@ -15,15 +16,16 @@ const bootloader        = new Bootloader();
 const thoregon = {};
 // *** some test methods
 Object.defineProperties(thoregon, {
-    'ui':               { value: false, configurable: false, enumerable: true, writable: false},
-    'isBrowser':        { value: false, configurable: false, enumerable: true, writable: false},
-    'isReliant':        { value: false, configurable: false, enumerable: true, writable: false},
-    'isNode':           { value: true,  configurable: false, enumerable: true, writable: false},
-    'isSovereign':      { value: true,  configurable: false, enumerable: true, writable: false},
-    'bootloader':       { value: bootloader,  configurable: false, enumerable: true, writable: false},
-    'nature':           { value: 'sovereign', configurable: false, enumerable: true, writable: false },
-    'density':          { value: 'headless',  configurable: false, enumerable: true, writable: false }, // todo: add 'headed' for Electron and mobile apps
-    'isThoregonModule': { value: (specifier) => bootloader.isThoregonModule(specifier), configurable: false, enumerable: true, writable: false}
+    'ui'              : { value: false, configurable: false, enumerable: true, writable: false },
+    'isBrowser'       : { value: false, configurable: false, enumerable: true, writable: false },
+    'isReliant'       : { value: false, configurable: false, enumerable: true, writable: false },
+    'isNode'          : { value: true, configurable: false, enumerable: true, writable: false },
+    'isSovereign'     : { value: true, configurable: false, enumerable: true, writable: false },
+    'bootloader'      : { value: bootloader, configurable: false, enumerable: true, writable: false },
+    'nature'          : { value: 'sovereign', configurable: false, enumerable: true, writable: false },
+    'density'         : { value: 'headless', configurable: false, enumerable: true, writable: false }, // todo: add 'headed' for Electron and mobile apps
+    'isThoregonModule': { value: (specifier) => bootloader.isThoregonModule(specifier), configurable: false, enumerable: true, writable: false },
+    'addLoader'       : { value: (name, loader, sig) => bootloader.addLoader(name, loader, sig), configurable: false, enumerable: true, writable: false },
 });
 
 /*
@@ -31,15 +33,24 @@ Object.defineProperties(thoregon, {
  */
 const properties = {};
 
-if (!global.thoregon)   properties.thoregon =   { value: thoregon,  configurable: false, enumerable: true, writable: false };
+if (!global.thoregon)   properties.thoregon   = { value: thoregon,  configurable: false, enumerable: true, writable: false };
 if (!global.globalThis) properties.globalThis = { value: global,    configurable: false, enumerable: true, writable: false };
+if (!global.crypto)     properties.crypto     = { value: { ...crypto.webcrypto.subtle, getRandomValues: crypto.webcrypto.getRandomValues },    configurable: false, enumerable: true, writable: false };
 
 Object.defineProperties(global, properties);
 
-export async function resolve(specifier, parentModuleURL = baseURL, defaultResolve) {
-    return bootloader.resolve(specifier, parentModuleURL, defaultResolve);
+export async function resolve(specifier, context, defaultResolve) {
+    return await bootloader.resolve(specifier, context, defaultResolve);
 }
 
-export async function dynamicInstantiate(url) {
-    return bootloader.dynamicInstantiate(url);
+export async function getFormat(href, context, defaultGetFormat) {
+    return await bootloader.getFormat(href, context, defaultGetFormat)
+}
+
+export async function getSource(url, context, defaultGetSource) {
+    return await bootloader.getSource(url, context, defaultGetSource);
+}
+
+export async function transformSource(source, context, defaultTransformSource) {
+    return await bootloader.transformSource(source, context, defaultTransformSource)
 }
--- doc/loaders.md
+++ doc/loaders.md
@@ -3,6 +3,60 @@ Loaders
 
 !! im browser require permissions from user: files system, push messages
 
+## resolving 
+
+- builtin
+    - node only, get builtin modules like fs, path, process, ...
+    - provide essential modules like path also for browser, use e.g. from browserify reimplementations 
+    
+- universe
+    - import an object/value from the universe
+    - import will wait until the object exists
+    - supports timeout for not found
+
+- thoregon
+    - basic environment information
+
+- global
+    - inmport object/value from global, 
+
+- @components
+    - deprecated, reference components via repository
+    - add a 'components.mjs' at the root of a node app and specifiy a mappiing to used components
+    - the modules in the 'components' folder will be mapped automatically
+
+- data: urls
+    - standard resolve in browser
+    - check for node -> https://nodejs.org/api/esm.html#esm_data_imports
+
+- node_modules
+    - node only, support node standard
+        - use only for infrastructure e.g. express, greenlock, ...
+    - no browser substitute
+
+- dorifer/repo
+    - resolve import specifier to the required version
+        - component descriptor specifies repo and components dependencies
+    - SSI always have control over used/referenced components and versions
+        - user can use different components/versions on each device 
+    - components can be redirected by users to replacements
+    - content in IPFS 
+    - service agent also have repo mappings and a device 
+    - node: resolve to the required components version URL
+    - browser: redirect to the required components version URL
+
+- Vault
+    - needs user interaction
+    - async encryption SSI and service agent 
+
+- tdev
+    - directory mappings
+    - additional version mappings
+
+Boot
+- first from filesystem (IPFS) for boot
+- later fetch updates from dorifer/repo
+
 ## Loader hierarchy
 
 higher level loaders are responsible to resolve the resource (location)
@@ -20,8 +74,6 @@ add code to define the scope (context) depending on the component
     - indexDB, window, document, Worker, SharedWorker, ... (TBD)
 - override all not redefined global with empty object
   
-firewalling will be done in 'getSource'  
-
 API:
 - async resolve(specifier, context)
     specifier   ... the requested resource
@@ -41,7 +93,11 @@ API:
     source      ... the source of the module
     context     ... url, format
     return: { source: source <string> | <SharedArrayBuffer> | <Uint8Arr | <ReadableStream | <AsyncIterator> }
-    
+  
+## Firewall
+
+firewalling will be done in 'getSource'  
+  
 
 ## Nodeloader
 
