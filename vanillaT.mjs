fb921f220e2dff12b510e1560f02e9fdb5f0cd25
Bernhard Lukassen <bl@bernhard-lukassen.com>
2022-05-19 12:36:42 +0200

    introduce vanillaT - CSV

--- vanillaT.mjs
+++ vanillaT.mjs
@@ -8,8 +8,22 @@
 
 import * as util from "/evolux.util";
 
-export const vanillaT = {
+//
+// globals available functions and entities
+//
+
+export const vanillaT͛ = {
     parseCSV: util.parseCSV
 }
 
-export const installT͛ = () => Object.entries(vanillaT).forEach(([name, fn]) => universe.global(name, fn, { doThrow: false }));
+export const installT͛ = () => Object.entries(vanillaT͛).forEach(([name, fn]) => universe.global(name, fn, { doThrow: false }));
+
+//
+// polyfills for Object, Array and older environemnts
+//
+
+
+
+//
+// browser polyfills (HTML)Element
+//
